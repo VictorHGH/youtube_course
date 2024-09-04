72aecc1f531d879e80cb92092f279e97eb4cd2c5
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-09-04 13:42:21 -0600

    [New] condicionales

--- index.php
+++ index.php
@@ -2,21 +2,92 @@
 
 declare(strict_types=1);
 
-// Operadores de asignación
-// =, +=, -=, *=, /=, %=
+// Condicionales
+// if, else, elseif, switch, ternario, match 
 
-echo $variable = 10;
-echo "\n";
-echo $variable += 5; // $variable = $variable + 5;
-echo "\n";
-echo $variable -= 5; // $variable = $variable - 5;
-echo "\n";
-echo $variable *= 5; // $variable = $variable * 5;
-echo "\n";
-echo $variable /= 5; // $variable = $variable / 5;
-echo "\n";
-echo $variable %= 5; // $variable = $variable % 5;
+// if
+echo "if";
+echo "<br>";
+$edad = 18;
+if ($edad >= 18) {
+	echo 'Eres mayor de edad';
+} else {
+	echo 'Eres menor de edad';
+}
 
+echo "<br>";
+echo "elseif";
+echo "<br>";
+
+// elseif
+$hora = 20;
+if ($hora < 12) {
+	echo 'Buenos días';
+} elseif ($hora < 20) {
+	echo 'Buenas tardes';
+} else {
+	echo 'Buenas noches';
+}
+
+echo "<br>";
+echo "switch";
+echo "<br>";
+
+// switch
+$dia = 3;
+switch ($dia) {
+	case 1:
+		echo 'Lunes';
+		break;
+	case 2:
+		echo 'Martes';
+		break;
+	case 3:
+		echo 'Miércoles';
+		break;
+	case 4:
+		echo 'Jueves';
+		break;
+	case 5:
+		echo 'Viernes';
+		break;
+	case 6:
+		echo 'Sábado';
+		break;
+	case 7:
+		echo 'Domingo';
+		break;
+	default:
+		echo 'No es un día de la semana';
+		break;
+}
+
+echo "<br>";
+echo "ternario";
+echo "<br>";
+
+// ternaario
+$edad = 18;
+$mensaje = $edad >= 18 ? 'Eres mayor de edad' : 'Eres menor de edad';
+echo $mensaje;
+
+echo "<br>";
+echo "match";
+echo "<br>";
+
+// match
+$dia = 5;
+$dia = match ($dia) {
+	1 => 'Lunes',
+	2 => 'Martes',
+	3 => 'Miércoles',
+	4 => 'Jueves',
+	5 => 'Viernes',
+	6 => 'Sábado',
+	7 => 'Domingo',
+	default => 'No es un día de la semana',
+};
+echo $dia;
 
 ?>
 
