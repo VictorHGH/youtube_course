970c6ed1177b97ae2512fab9ee20d2981ddc8653
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-09-19 15:02:20 -0600

    [Finished] estructuras repetitivas clase 1

--- index.php
+++ index.php
@@ -2,95 +2,19 @@
 
 declare(strict_types=1);
 
-//Siguiente capitulo:
-// https://www.youtube.com/watch?v=3HmFLO29RPU&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=12
+// Estructuras repetitivas
+$base = 5;
+$exp = 4;
 
-// Condicionales
-// if, else, elseif, switch, ternario, match 
+$resultado = 1;
+$i = 1;
 
-// if
-echo "if";
-echo "<br>";
-$edad = 18;
-if ($edad >= 18) {
-	echo 'Eres mayor de edad';
-} else {
-	echo 'Eres menor de edad';
+while ($i <= $exp) {
+	$resultado *= $base;
+	$i++;
 }
 
-echo "<br>";
-echo "elseif";
-echo "<br>";
-
-// elseif
-$hora = 20;
-if ($hora < 12) {
-	echo 'Buenos días';
-} elseif ($hora < 20) {
-	echo 'Buenas tardes';
-} else {
-	echo 'Buenas noches';
-}
-
-echo "<br>";
-echo "switch";
-echo "<br>";
-
-// switch
-$dia = 3;
-switch ($dia) {
-	case 1:
-		echo 'Lunes';
-		break;
-	case 2:
-		echo 'Martes';
-		break;
-	case 3:
-		echo 'Miércoles';
-		break;
-	case 4:
-		echo 'Jueves';
-		break;
-	case 5:
-		echo 'Viernes';
-		break;
-	case 6:
-		echo 'Sábado';
-		break;
-	case 7:
-		echo 'Domingo';
-		break;
-	default:
-		echo 'No es un día de la semana';
-		break;
-}
-
-echo "<br>";
-echo "ternario";
-echo "<br>";
-
-// ternaario
-$edad = 18;
-$mensaje = $edad >= 18 ? 'Eres mayor de edad' : 'Eres menor de edad';
-echo $mensaje;
-
-echo "<br>";
-echo "match";
-echo "<br>";
-
-// match
-$dia = 5;
-$dia = match ($dia) {
-	1 => 'Lunes',
-	2 => 'Martes',
-	3 => 'Miércoles',
-	4 => 'Jueves',
-	5 => 'Viernes',
-	6 => 'Sábado',
-	7 => 'Domingo',
-	default => 'No es un día de la semana',
-};
-echo $dia;
+echo "El resultado es: " . $resultado;
 
 ?>
 
