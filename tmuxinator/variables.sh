a94971c8583eceff1b9dcd0944f547a5d10538c6
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-09-19 15:05:17 -0600

    [Updated] veriables script for new video

--- tmuxinator/variables.sh
+++ tmuxinator/variables.sh
@@ -4,11 +4,11 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
 	link1="localhost:8000"
-	link2="https://www.youtube.com/watch?v=3HmFLO29RPU&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=12"
+	link2="https://www.youtube.com/watch?v=JyL7z3_9-PU&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=13"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Linux
 	link1="localhost/youtube_course"
-	link2="https://www.youtube.com/watch?v=3HmFLO29RPU&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=12"
+	link2="https://www.youtube.com/watch?v=JyL7z3_9-PU&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=13"
 else
     echo "Unsupported operating system"
     exit 1
