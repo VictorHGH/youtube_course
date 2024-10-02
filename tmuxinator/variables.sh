e6858632c52ec27feede811b3a910fc9cac4b1b1
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-10-02 14:16:49 -0600

    [Updated] variables file for only use one string for video classes

--- tmuxinator/variables.sh
+++ tmuxinator/variables.sh
@@ -4,12 +4,12 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
 	link1="localhost:8000"
-	link2="https://www.youtube.com/watch?v=MO93xlhq5ZE&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=15"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Linux
 	link1="localhost/youtube_course"
-	link2="https://www.youtube.com/watch?v=MO93xlhq5ZE&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=15"
 else
     echo "Unsupported operating system"
     exit 1
 fi
+
+link2="https://www.youtube.com/watch?v=MO93xlhq5ZE&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=15"
