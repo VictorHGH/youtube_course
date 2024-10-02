27d4fd7ad0bc1a976888069f3da874601cd9c527
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-10-02 14:15:17 -0600

    [Updated] variables for new class

--- tmuxinator/variables.sh
+++ tmuxinator/variables.sh
@@ -4,11 +4,11 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
 	link1="localhost:8000"
-	link2="https://www.youtube.com/watch?v=UA1ADyiZFR8&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=14"
+	link2="https://www.youtube.com/watch?v=MO93xlhq5ZE&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=15"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Linux
 	link1="localhost/youtube_course"
-	link2="https://www.youtube.com/watch?v=UA1ADyiZFR8&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=14"
+	link2="https://www.youtube.com/watch?v=MO93xlhq5ZE&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=15"
 else
     echo "Unsupported operating system"
     exit 1
