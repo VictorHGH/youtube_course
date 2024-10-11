d102e807bc105376217a5662f3554a80aa6bdff4
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-10-11 13:40:02 -0600

    [Finished] class of arrays unidimensionales

---
+++ 02_arrays_unidimensionales.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+$array = [
+	'nombre' => 'Victor Arana Flores',
+	'email' => 'victor@codersfree.com',
+	'celular' => '+529988888888'
+];
+
+echo $array['email'];
