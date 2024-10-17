23d682ae11a8c427ea7db5fcdf880834a82e9665
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-10-17 13:34:13 -0600

    [Finished] first class of oop

--- 06_oop/clases/personas.php
+++ 06_oop/clases/personas.php
@@ -2,7 +2,27 @@
 
 declare(strict_types=1);
 
-class Persona {
-	public string $nombre, $apellido;
-	public int $edad;
+class Persona
+{
+	public function __construct(
+		private string $nombre,
+		public string $apellido,
+		public int $edad,
+	) {
+		$this->nombre = strtolower($nombre);
+		$this->apellido = strtolower($apellido);
+	}
+
+	public function setNombre(string $nombre): string
+	{
+		$this->nombre = strtolower($nombre);
+
+		return $this->nombre;
+	}
+
+	public function getNombre(): string
+	{
+		$this->nombre = ucwords($this->nombre);
+		return $this->nombre;
+	}
 }
--- 06_oop/index.php
+++ 06_oop/index.php
@@ -1,3 +1,22 @@
 <?php
 
 declare(strict_types=1);
+
+require_once 'clases/personas.php';
+
+$persona1 = new Persona('VicTor HUGO', 'Arana', 25);
+
+/* $persona1->setNombre('Victor hugO');
+$persona1->apellido = 'González';
+$persona1->edad = 25; */
+
+
+$persona2 = new Persona('Pamela', 'Montes', 27);
+
+/* $persona2->setNombre('Pamela');
+$persona2->apellido = 'Montes';
+$persona2->edad = 27; */
+
+echo "El nombre de la persona 1 es: " . $persona1->getNombre();
+echo "<br>";
+echo "El nombre de la persona 2 es: " . $persona2->getNombre();
