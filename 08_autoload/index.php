e07a52d2d8b8c080bd08e9816027f7136989d25c
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-01-21 13:03:02 -0600

    [Fixed] if statmant in autoload class for index.php file

--- 08_autoload/index.php
+++ 08_autoload/index.php
@@ -4,7 +4,7 @@ use Controllers\CourseController;
 use Models\Course;
 
 spl_autoload_register(function($clase){
-	if (file_exists(require_once str_replace('\\', '/', $clase) . ".php")){
+	if (file_exists(str_replace('\\', '/', $clase) . ".php")){
 		require_once str_replace('\\', '/', $clase) . ".php";
 	}
 });
