3825db0b4c3c183d39040890f2279ff6186e1f45
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-01-27 23:24:01 -0600

    [Finished] regular expretions class

--- 13_expresiones_regulares/index.php
+++ 13_expresiones_regulares/index.php
@@ -1,1 +1,30 @@
 <?php
+
+$cadena = "Holaaaaaaaa";
+
+$expresion = "/hola*/i";
+
+echo "<h1>Expresiones regulares</h1>";
+
+if (preg_match($expresion, $cadena)) {
+	echo "La cadena contiene la expresion";
+} else {
+	echo "La cadena NO contiene la expresion";
+}
+
+echo "<h2>Expresiones regulares con patrones</h2>";
+
+$url = "https://youtu.be/xGaXH7spCyA?si=kNgCYsi6OREU050y";
+
+$patron = "%^(?:https://)?(?:www\.)?(?:youtu\.be/|youtube\.com/watch\?v=)(\w{10,12})%i";
+
+if (preg_match($patron, $url, $matches)) {
+	echo "La url es valida";
+} else {
+	echo "La url NO es valida";
+}
+
+echo "<br>";
+echo "<br>";
+
+echo "El id es: " . $matches[1];
