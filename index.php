163f6302e73ec1147a09be524368ecfc88afcdd4
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-01-28 10:19:25 -0600

    [Updated] index.php for better welcome page

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<h1>Hola Mundo</h1>
+<div class="container">
+	<h1>Youtube Course</h1>
+	<main class="container">
+		<a href="https://www.youtube.com/playlist?list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg" target="_blank">
+			<img src="./imagenes/playlist.avif" alt="Imagen de la playlist de youtube">
+		</a>
+		<a class="next-class" href="./14_sql/">Next class -></a>
+	</main>
+</div>
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 80%;
+		margin: auto;
+	}
+
+	.next-class {
+		margin: 30px 0 0 0;
+		text-decoration: none;
+		color: black;
+		font-weight: bold;
+		background-color: orange;
+		padding: 10 20px;
+		border-radius: 10px;
+	}
+</style>
