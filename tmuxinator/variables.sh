28089b6afed269e429d9410e9307acefe51681b7
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-01-30 12:37:08 -0600

    [Updated] sql class with schema and link for next video

--- tmuxinator/variables.sh
+++ tmuxinator/variables.sh
@@ -12,7 +12,7 @@ else
     exit 1
 fi
 
-link2="https://www.youtube.com/watch?v=rlvtPkEH72U&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=43"
+link2="https://www.youtube.com/watch?v=uqrqrDRgXtE&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=48"
 
 export link1
 export link2
