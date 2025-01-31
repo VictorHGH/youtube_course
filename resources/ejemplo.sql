868648b0755c0468e73fce9d1f38bbf4ce1c2fae
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-01-30 23:54:19 -0600

    [Updated] sql file

--- resources/ejemplo.sql
+++ resources/ejemplo.sql
@@ -1,78 +1,22 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 30-01-2025 a las 19:34:35
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `ejemplo`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `contactos`
---
-
-CREATE TABLE `contactos` (
-  `id` int(11) NOT NULL,
+CREATE TABLE ejemplo.`contactos` (
+  `id` int NOT NULL,
   `nombre` varchar(64) NOT NULL,
   `apellidos` varchar(64) NOT NULL,
   `direccion` varchar(256) NOT NULL,
   `telefono` varchar(10) NOT NULL,
   `correo` varchar(100) NOT NULL,
   `fecha_nacimiento` date DEFAULT NULL,
-  `familia` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `familia` boolean NOT NULL,
+  primary key(id),
+  index(nombre),
+  index(apellidos),
+  fulltext(direccion)
+);
 
---
--- Volcado de datos para la tabla `contactos`
---
-
-INSERT INTO `contactos` (`id`, `nombre`, `apellidos`, `direccion`, `telefono`, `correo`, `fecha_nacimiento`, `familia`) VALUES
+INSERT INTO ejemplo.`contactos` (`id`, `nombre`, `apellidos`, `direccion`, `telefono`, `correo`, `fecha_nacimiento`, `familia`) VALUES
 (1, 'Pamela', 'Arana Flores', 'Calle falsa 123', '5582004034', 'pamela@gmail.com', '1904-12-16', 0),
 (2, 'Iris', 'Godoy', 'Calle falsa 456', '5530459258', 'iris@hotmail.com', '1992-02-12', 1),
 (3, 'Irma', 'Flores Fuentes', 'Calle falsa 3493', '5530483713', 'irma@outlook.com', '1990-03-11', 0),
 (4, 'Joel', 'Cordova', 'Calle falsa 3013', '5539294956', 'joel@gmail.com', '1996-01-01', 1),
 (5, 'Zolia', 'Flores Peche', 'Calle falsa 3345', '5530130460', 'zolia@outlook.com', '1991-02-03', 0);
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `contactos`
---
-ALTER TABLE `contactos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `nombre` (`nombre`),
-  ADD KEY `apellidos` (`apellidos`);
-ALTER TABLE `contactos` ADD FULLTEXT KEY `direccion` (`direccion`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `contactos`
---
-ALTER TABLE `contactos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
