1fc86bacbea8c780db1133bd1040a8e568e7cb16
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-02-13 10:18:51 -0600

    [Updated] router

--- 15_mvc/lib/Route.php
+++ 15_mvc/lib/Route.php
@@ -6,16 +6,28 @@ class Route {
 	private static $routes = [];
 
 	public static function get($uri, $callback){
+		$uri = trim($uri, '/');
 		self::$routes['GET'][$uri] = $callback;
 	}
 
 	public static function post($uri, $callback){
+		$uri = trim($uri, '/');
 		self::$routes['POST'][$uri] = $callback;
 	}
 
 	public static function dispatch(){
 		$uri = $_SERVER['REQUEST_URI'];
+		$uri = trim($uri, '/');
 
-		echo "URI: $uri<br>";
+		$method = $_SERVER['REQUEST_METHOD'];
+
+		foreach(self::$routes[$method] as $route => $callback){
+			if ($route === $uri){
+				$callback();
+				return;
+			} 
+		}
+
+		echo "404 Not Found";
 	}
 }
