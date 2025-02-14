fccab049dd233adccb3345c112cdf0a035787b99
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-02-14 13:05:42 -0600

    [Created] file for home

---
+++ 15_mvc/resources/views/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Home Page</title>
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<h1>Hola desde la página de Home</h1>
+		<p>Título: <?= $title ?></p>
+		<p>Descripción: <?= $description ?></p>
+	</body>
+
+</html>
