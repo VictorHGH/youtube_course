f486685dfe2368027e7bdd920284d9d9b4db4b1f
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-02-17 14:42:45 -0600

    [Updated] Contact model for use of a general model

--- 15_mvc/app/Models/Contact.php
+++ 15_mvc/app/Models/Contact.php
@@ -2,28 +2,8 @@
 
 namespace app\Models;
 
-use mysqli;
+class Contact extends Model {
 
-class Contact {
+	protected $table = 'contacts';
 
-	protected $db_host = DB_HOST;
-	protected $db_user = DB_USER;
-	protected $db_pass = DB_PASS;
-	protected $db_name = DB_NAME;
-
-	protected $connection;
-
-	public function __construct() {
-		$this->connection();
-	}
-
-	public function connection() {
-
-		$this->connection = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
-
-		if ($this->connection->connect_error) {
-			die('No se pudo conectar: ' . $this->connection->connect_error);
-		}
-
-	}
 }
