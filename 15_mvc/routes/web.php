8b1e96a8ef7393999c63beaf9248cb474ceff7ae
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-02-20 15:07:54 -0600

    [Created] new routes

--- 15_mvc/routes/web.php
+++ 15_mvc/routes/web.php
@@ -2,19 +2,16 @@
 
 use lib\Route;
 use app\Controllers\HomeController;
+use app\Controllers\ContactsController;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/contact', function(){
-	return 'Hola desde la página contact';
-});
-
-Route::get('/about', function(){
-	return 'Hola desde la página about';
-});
-
-Route::get('/courses/:slug', function($slug){
-	return 'El curso es: ' . $slug;
-});
+Route::get('/contacts', [ContactsController::class, 'index']);
+Route::get('/contacts/create', [ContactsController::class, 'create']);
+Route::get('/contacts/:id', [ContactsController::class, 'show']);
+Route::get('/contacts/:id/edit', [ContactsController::class, 'edit']);
+Route::post('/contacts', [ContactsController::class, 'store']);
+Route::post('/contacts/:id', [ContactsController::class, 'update']);
+Route::post('/contacts/:id/delete', [ContactsController::class, 'destroy']);
 
 Route::dispatch();
