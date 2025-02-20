47e97dde164cb057a00f4fb85e2502acb8c4260d
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-02-20 15:08:54 -0600

    [Updated] home controller

--- 15_mvc/app/Controllers/HomeController.php
+++ 15_mvc/app/Controllers/HomeController.php
@@ -2,15 +2,9 @@
 
 namespace app\Controllers;
 
-use app\Models\Contact;
-
 class HomeController extends Controller {
 
 	public function index() {
-
-		$contactModel = new Contact();
-
-		return $contactModel->delete(13);
 
 		return $this->view('home', [
 			'title' => 'Home Page',
