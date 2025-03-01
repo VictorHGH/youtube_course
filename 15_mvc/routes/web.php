3c7e204025864630540fcf5a19cf1a2ddbf658d0
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-03-01 01:29:26 -0600

    [Updated] routes

--- 15_mvc/routes/web.php
+++ 15_mvc/routes/web.php
@@ -7,11 +7,11 @@ use app\Controllers\ContactsController;
 NewRoute::get('/', [HomeController::class, 'index']);
 
 NewRoute::get('/contacts', [ContactsController::class, 'index']);
-/* Route::get('/contacts/create', [ContactsController::class, 'create']);
-Route::get('/contacts/:id', [ContactsController::class, 'show']);
-Route::get('/contacts/:id/edit', [ContactsController::class, 'edit']);
-Route::post('/contacts', [ContactsController::class, 'store']);
-Route::post('/contacts/:id', [ContactsController::class, 'update']);
-Route::post('/contacts/:id/delete', [ContactsController::class, 'destroy']); */
+/* Route::get('/contacts/create', [ContactsController::class, 'create']); */
+NewRoute::get('/contacts/:id', [ContactsController::class, 'show']);
+/* Route::get('/contacts/:id/edit', [ContactsController::class, 'edit']); */
+/* Route::post('/contacts', [ContactsController::class, 'store']); */
+/* Route::post('/contacts/:id', [ContactsController::class, 'update']); */
+/* Route::post('/contacts/:id/delete', [ContactsController::class, 'destroy']); */
 
 NewRoute::dispatch();
